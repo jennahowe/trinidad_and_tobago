adfcbf23d129404402a71653aeaa09c1d7f6259b
Jenna Howe <jenna.howe@live.co.uk>
2015-10-17 00:51:47 +0100

    Add image

--- scraper.py
+++ scraper.py
@@ -4,11 +4,18 @@ from urllib import urlretrieve
 import re
 import scraperwiki
 import pdb
+import time
 
+sleep_between_requests = 5 # (seconds)
 
 def scrape_person(person_url):
     page = urlopen(person_url)
     soup = BeautifulSoup(page)
+
+    image = soup.findAll("img", {"class": "img"})[0]
+    image = re.search(r'src="(?P<url>.+)"\s', str(image)).group('url')
+
+    return image
 
 def scrape_list():
     base_url = "http://ttparliament.org"
@@ -27,12 +34,14 @@ def scrape_list():
         id_ = url_match.group('id')
         faction = re.search(r'<td width="140">(?P<faction>.*)</td>', str(row)).group('faction')
         constituency = re.search(r'<td width="200">(?P<constituency>.*)</td>', str(row)).group('constituency')
+
         name = re.search(r'">(?P<name>.+)</a>', str(row)).group('name')
         name = name.split(', ')
+
         family_name = name[0]
         given_name = name[1]
         name = "%s %s" % (name[1], name[0])
-        # image = scrape_person(person_url)
+        image = base_url + scrape_person(person_url)
 
         data = {
            'id': id_,
@@ -42,8 +51,12 @@ def scrape_list():
            'source': person_url,
            'faction': faction,
            'constituency': constituency,
+           'image': image,
         }
         print data, "\n"
         scraperwiki.sql.save(unique_keys=['id'], data=data)
+
+        time.sleep(sleep_between_requests)
+
 
 scrape_list()
